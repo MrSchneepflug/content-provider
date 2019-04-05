3b3738a08a1998a9fa8985b084b1ce3a2ba1d76b
Mr. Schneepflug <mrschneepflug@not-my-problem.de>
2019-04-05 17:22:23 +0200

    2.1.0

--- lib/factories.ts
+++ lib/factories.ts
@@ -10,7 +10,7 @@ export function createExpressApplication(config: ConfigInterface, database: Data
 
   app.use(cors());
   app.use(healthRoutes());
-  app.use(contentRoutes(config.logger, config, database));
+  app.use(contentRoutes(config, database));
 
   return app;
 }
